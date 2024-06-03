c0aa30bf6addeb93ea2680d5a56cd8cf25e369b5
YingXing <yingxing.cheng@mathematik.uni-stuttgart.de>
2024-06-03 13:36:30 +0200

    fix tests data

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -1,7 +1,9 @@
+import importlib.resources as pkg_resources
 import tempfile
 
 import pytest
 
+import tests.data  # Ensure your tests/data is a Python package (i.e., it contains an __init__.py file)
 from pydirac_papertools.processor import (
     AtomDataProcessor,
     DCCCDataProcessor,
@@ -12,65 +14,73 @@ from pydirac_papertools.processor import (
 )
 
 
-@pytest.mark.parametrize("data_path, element", [("data/Al_nr", "Al")])
+@pytest.mark.parametrize("data_path, element", [("Al_nr", "Al")])
 def test_NRCCDataProcessor(data_path, element):
-    np = NRCCDataProcessor(data_path, element)
-    print(np.find_best())
+    with pkg_resources.path(tests.data, data_path) as data_file:
+        np = NRCCDataProcessor(str(data_file), element)
+        print(np.find_best())
 
 
 @pytest.mark.parametrize(
     "data_path, element, is_quad",
-    [("data/Al", "Al", False)],
+    [("Al", "Al", False)],
 )
 def test_SRCCDataProcessor(data_path, element, is_quad):
-    sp_d = SRCCDataProcessor(data_path, element, is_quad=is_quad)
-    print(sp_d.as_dict())
+    with pkg_resources.path(tests.data, data_path) as data_file:
+        sp_d = SRCCDataProcessor(str(data_file), element, is_quad=is_quad)
+        print(sp_d.as_dict())
 
 
-@pytest.mark.parametrize("data_path, element, is_quad", [("data/Al_so", "Al", False)])
+@pytest.mark.parametrize("data_path, element, is_quad", [("Al_so", "Al", False)])
 def test_SOCCDataProcessor(data_path, element, is_quad):
-    sp = DCCCDataProcessor(data_path, element, is_quad=is_quad)
-    sp.load_data()
-    print(sp.polar)
+    with pkg_resources.path(tests.data, data_path) as data_file:
+        sp = DCCCDataProcessor(str(data_file), element, is_quad=is_quad)
+        sp.load_data()
+        print(sp.polar)
 
 
-@pytest.mark.parametrize("data_path, element, is_quad", [("data/O_mrci", "O", False)])
+@pytest.mark.parametrize("data_path, element, is_quad", [("O_mrci", "O", False)])
 def test_SOCIDataProcessor(data_path, element, is_quad):
-    sp = DCCIDataProcessor(data_path, element, is_quad=is_quad)
-    sp.load_data()
-    print(sp.find_best())
+    with pkg_resources.path(tests.data, data_path) as data_file:
+        sp = DCCIDataProcessor(str(data_file), element, is_quad=is_quad)
+        sp.load_data()
+        print(sp.find_best())
 
 
-@pytest.mark.parametrize("data_path, element, is_quad", [("data", "Al", False)])
+@pytest.mark.parametrize("data_path, element, is_quad", [(".", "Al", False)])
 def test_AtomDataProcessor(data_path, element, is_quad):
-    adp = AtomDataProcessor(data_path, element, is_quad=is_quad)
-    print(adp.analysis())
+    with pkg_resources.path(tests.data, data_path) as data_file:
+        adp = AtomDataProcessor(str(data_file), element, is_quad=is_quad)
+        print(adp.analysis())
 
 
-@pytest.mark.parametrize("file_path", [("data/group-1.json")])
+@pytest.mark.parametrize("file_path", [("group-1.json")])
 def test_GroupDataProcessor(file_path):
-    gdp = GroupDataProcessor.from_file(file_path)
-    print(gdp)
-    gdp.summary()
+    with pkg_resources.path(tests.data, file_path) as data_file:
+        gdp = GroupDataProcessor.from_file(str(data_file))
+        print(gdp)
+        gdp.summary()
 
 
 # New tests with temporary directory for caching
-@pytest.mark.parametrize("data_path, element, is_quad", [("data/Al_so", "Al", False)])
+@pytest.mark.parametrize("data_path, element, is_quad", [("Al_so", "Al", False)])
 def test_SOCCDataProcessor_to_file(data_path, element, is_quad):
-    sp = DCCCDataProcessor(data_path, element, is_quad=is_quad)
-    sp.load_data()
-    with tempfile.TemporaryDirectory() as temp_dir:
-        temp_file = f"{temp_dir}/tmp.json"
-        sp.to_file(temp_file)
-        sp2 = DCCCDataProcessor.from_file(temp_file)
-        print(sp2.polar)
+    with pkg_resources.path(tests.data, data_path) as data_file:
+        sp = DCCCDataProcessor(str(data_file), element, is_quad=is_quad)
+        sp.load_data()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_file = f"{temp_dir}/tmp.json"
+            sp.to_file(temp_file)
+            sp2 = DCCCDataProcessor.from_file(temp_file)
+            print(sp2.polar)
 
 
-@pytest.mark.parametrize("data_path, element, is_quad", [("data", "Al", False)])
+@pytest.mark.parametrize("data_path, element, is_quad", [(".", "Al", False)])
 def test_AtomDataProcessor_to_file(data_path, element, is_quad):
-    adp = AtomDataProcessor(data_path, element, is_quad=is_quad)
-    with tempfile.TemporaryDirectory() as temp_dir:
-        temp_file = f"{temp_dir}/Al.json"
-        adp.to_file(temp_file)
-        adp = AtomDataProcessor.from_file(temp_file)
-        print(adp.analysis())
+    with pkg_resources.path(tests.data, data_path) as data_file:
+        adp = AtomDataProcessor(str(data_file), element, is_quad=is_quad)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_file = f"{temp_dir}/Al.json"
+            adp.to_file(temp_file)
+            adp = AtomDataProcessor.from_file(temp_file)
+            print(adp.analysis())
