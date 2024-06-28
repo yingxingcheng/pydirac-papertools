bad4f5ddef775b89a38abd650343538385163d8a
YingXing <yingxing.cheng@mathematik.uni-stuttgart.de>
2024-06-28 21:42:34 +0200

    fix bug

--- src/pydirac_papertools/processor/methods/dcci.py
+++ src/pydirac_papertools/processor/methods/dcci.py
@@ -62,7 +62,9 @@ class DCCIDataProcessor(AtomAbstractDataProcessor):
                 pass
             elif gp in [13, 14, 15, 16, 17]:
                 param_dict.update(self.get_extra_states(v, precision))
-                param_dict.update(self.get_extra_states_error(errors, precision))
+                extra_states_error = self.get_extra_states_error(errors, precision)
+                tmp_dict = {f"{k}_error": v for k, v in extra_states_error.items()}
+                param_dict.update(tmp_dict)
                 if gp in [13]:
                     param_dict["diff"] = param_dict["P_3/2"] - param_dict["gs"]
                 elif gp in [17]:
@@ -131,21 +133,20 @@ class DCCIDataProcessor(AtomAbstractDataProcessor):
         elif gp in [2, 12, 14, 18]:
             gs = entity["sym_1_root_1"][pos]
         elif gp in [15]:
-            gs = np.sqrt(
-                0.5 * entity["sym_3_root_1"][pos] ** 2 + 0.5 * entity["sym_3_root_2"][pos] ** 2
-            )
+            gs = np.sqrt((entity["sym_3_root_1"][pos] ** 2 + entity["sym_3_root_2"][pos] ** 2) / 2)
         elif gp in [16]:
             gs = np.sqrt(
-                0.2 * entity["sym_1_root_1"][pos] ** 2
-                + 0.2 * entity["sym_1_root_2"][pos] ** 2
-                + 0.2 * entity["sym_1_root_3"][pos] ** 2
-                + 0.2 * entity["sym_2_root_1"][pos] ** 2
-                + 0.2 * entity["sym_2_root_2"][pos] ** 2
+                (
+                    entity["sym_1_root_1"][pos] ** 2
+                    + entity["sym_1_root_2"][pos] ** 2
+                    + entity["sym_1_root_3"][pos] ** 2
+                    + entity["sym_2_root_1"][pos] ** 2
+                    + entity["sym_2_root_2"][pos] ** 2
+                )
+                / 5
             )
         elif gp in [17]:
-            gs = np.sqrt(
-                0.5 * entity["sym_3_root_1"][pos] ** 2 + 0.5 * entity["sym_3_root_2"][pos] ** 2
-            )
+            gs = np.sqrt((entity["sym_3_root_1"][pos] ** 2 + entity["sym_3_root_2"][pos] ** 2) / 2)
 
         else:
             raise NotImplementedError
@@ -199,32 +200,32 @@ class DCCIDataProcessor(AtomAbstractDataProcessor):
         gp = self.element.group
         if gp in [13]:
             d = {
-                "P_3/2_1/2_error": entity["sym_3_root_2"][pos],
-                "P_3/2_3/2_error": entity["sym_3_root_3"][pos],
-                "P_3/2_error": np.sqrt(
+                "P_3/2_1/2": entity["sym_3_root_2"][pos],
+                "P_3/2_3/2": entity["sym_3_root_3"][pos],
+                "P_3/2": np.sqrt(
                     0.5 * entity["sym_3_root_2"][pos] ** 2 + 0.5 * entity["sym_3_root_3"][pos] ** 2
                 ),
             }
         elif gp in [15]:
             d = {
-                "P_3/2_1/2_error": entity["sym_3_root_1"][pos],
-                "P_3/2_3/2_error": entity["sym_3_root_2"][pos],
+                "P_3/2_1/2": entity["sym_3_root_1"][pos],
+                "P_3/2_3/2": entity["sym_3_root_2"][pos],
             }
         elif gp in [16]:
             d = {
-                "P_2_0_error": entity["sym_1_root_1"][pos],
-                "P_2_1_error": np.sqrt(
+                "P_2_0": entity["sym_1_root_1"][pos],
+                "P_2_1": np.sqrt(
                     0.5 * entity["sym_1_root_2"][pos] ** 2 + 0.5 * entity["sym_1_root_3"][pos] ** 2
                 ),
-                "P_2_2_error": np.sqrt(
+                "P_2_2": np.sqrt(
                     0.5 * entity["sym_2_root_1"][pos] ** 2 + 0.5 * entity["sym_2_root_2"][pos] ** 2
                 ),
             }
         elif gp in [17]:
             d = {
-                "P_3/2_3/2_error": entity["sym_3_root_1"][pos],
-                "P_3/2_1/2_error": entity["sym_3_root_2"][pos],
-                "P_1/2_1/2_error": entity["sym_3_root_3"][pos],
+                "P_3/2_3/2": entity["sym_3_root_1"][pos],
+                "P_3/2_1/2": entity["sym_3_root_2"][pos],
+                "P_1/2_1/2": entity["sym_3_root_3"][pos],
             }
         elif gp in [1, 2, 11, 12, 14, 18]:
             d = {}
@@ -275,17 +276,19 @@ class DCCIDataProcessor(AtomAbstractDataProcessor):
             fields = entity["fields"]
             energies = entity["energies"]
             for i, field in enumerate(fields):
-                d = {"calc_type": ct, "Field": field}
-                for state, outs in energies.items():
-                    if state == "scf_e" and "SCF" not in d:
-                        d["SCF"] = outs[i]
-                    else:
-                        d["State"] = state
-                        d["CISD"] = outs[i]
-                d["Tag"] = tags[ct] if ct in tags else 0
                 if field > self.threshold:
                     continue
-                data_list.append(d)
+
+                # if SCF energy exists, this should be added as well.
+                new_energies = {k: v for k, v in energies.items() if k != "scf_e"}
+                for state, outs in new_energies.items():
+                    d = {"calc_type": ct, "Field": field}
+                    d["Tag"] = tags[ct] if ct in tags else 0
+                    d["State"] = state
+                    d["CISD"] = outs[i]
+                    if "scf_e" in energies.keys():
+                        d["SCF"] = energies["scf_e"][i]
+                    data_list.append(d)
         df = pd.DataFrame(data_list)
         df.set_index("calc_type", inplace=True)
         return df
@@ -337,13 +340,13 @@ class DCCIDataProcessor(AtomAbstractDataProcessor):
                 "calc_type": ct,
                 "State": "gs",
                 "SCF": p_scf,
-                "CISD": self.get_gs(entity),
+                "CISD": self.get_gs_error(entity),
             }
             d["Tag"] = tags[ct] if ct in tags else 0
             data_list.append(d)
 
             # results for extra states
-            extra_states = self.get_extra_states(entity)
+            extra_states = self.get_extra_states_error(entity)
             for s, v in extra_states.items():
                 d = {"calc_type": ct, "State": s, "SCF": p_scf, "CISD": v}
                 d["Tag"] = tags[ct] if ct in tags else 0
